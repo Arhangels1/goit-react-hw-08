376d115ad829e83dcb6f5ed9fba32d1ee2f13fc6
Arhangels1 <157172462+Arhangels1@users.noreply.github.com>
2024-09-16 22:15:20 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,16 @@
-import { Routes, Route } from "react-router-dom";
-import { lazy, Suspense } from "react";
+import ContactList from "./components/ContactList/ContactList";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactForm from "./components/ContactForm/ContactForm";
 
-import Navigation from "./components/Navigation/Navigation";
-import Loader from "./components/Loader/Loader";
-
-// import HomePage from "./pages/HomePage";
-// import MoviesPage from "./pages/MoviesPage";
-// import MovieDetailsPage from "./pages/MovieDetailsPage";
-// import MovieCast from "./components/MovieCast/MovieCast";
-// import MovieReviews from "./components/MovieReviews/MovieReviews";
-// import NotFoundPage from "./pages/NotFoundPage";
-
-const HomePage = lazy(() => import("./pages/HomePage"));
-const MoviesPage = lazy(() => import("./pages/MoviesPage"));
-const MovieDetailsPage = lazy(() => import("./pages/MovieDetailsPage"));
-const MovieCast = lazy(() => import("./components/MovieCast/MovieCast"));
-const MovieReviews = lazy(() =>
-  import("./components/MovieReviews/MovieReviews")
-);
-const NotFoundPage = lazy(() => import("./pages/NotFoundPage"));
-
-//import css from "./App.module.css";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <Navigation />
-      <Suspense fallback={<Loader />}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/movies" element={<MoviesPage />} />
-          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
-            <Route path="cast" element={<MovieCast />} />
-            <Route path="reviews" element={<MovieReviews />} />
-          </Route>
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </Suspense>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
     </div>
   );
 }
